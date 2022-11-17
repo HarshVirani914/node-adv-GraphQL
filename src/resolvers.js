9affc649ef59c350cb83a8d93cc13d85679287a5
harsh virani <harshvirani.91@gmail.com>
2022-11-17 14:52:26 +0530

    added-subsciption

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,5 +1,8 @@
 const { authenticated, authorized } = require("./auth");
+const { PubSub } = require("apollo-server");
 const NEW_POST = "NEW_POST";
+
+const pubsub = new PubSub();
 
 /**
  * Anything Query / Mutation resolver
@@ -76,6 +79,11 @@ module.exports = {
 
       const token = createToken(user);
       return { token, user };
+    },
+  },
+  Subscription: {
+    newPost: {
+      subscribe: () => pubsub.asyncIterator(NEW_POST),
     },
   },
   User: {
